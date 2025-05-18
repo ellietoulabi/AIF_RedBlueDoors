7a7d5ff18421f4022c2b50bc0ffe9a0c67428b45
Ellie <elitoulabin@gmail.com>
2025-05-17 18:26:13 -0600

    drac script added.

--- submitter.sh
+++ submitter.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#SBATCH --account=def-mbowling
+#SBATCH --account=def-jrwright
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
 #SBATCH --time=0-2:59
 #SBATCH --salloc cpu-freq=Performance
 
-# setup and tear down takes .5-2 minutes.
 module purge
 module load python/3.11.4  scipy-stack
 
@@ -14,31 +13,42 @@ if [ "$SLURM_TMPDIR" == "" ]; then
 fi
 
 cd $SLURM_TMPDIR
-
-
+echo "Moved to SLURM_TMPDIR"
 
 mkdir project
 mkdir virtualenvs
+echo "Created directories"
 
-cd project
+
 echo "Cloning repo..."
+cd project
 git clone --quiet https://github.com/ellietoulabi/AIF_RedBlueDoors.git 
+echo "Clone done"
 
 
+echo "Creating virtual environment..."
 cd ../virtualenvs
 python3.11 -m venv .venv
 source .venv/bin/activate
+echo "Virtual environment created"
 
+
+echo "Installing dependencies..."
 cd ../project/AIF_RedBlueDoors/
 pip install -r requirements.txt
+echo "Dependencies installed"
 
 
-cd runs
 echo "Running experiment..."
-
+cd runs
 python run_redbluedoors_ql_random.py
+echo "Experiment done"
 
-echo "done"
+
+echo "Copying logs to home directory..."
+cd ..
+cp -r logs /home/toulabin/projects/def-jrwright/toulabin/AIF_RedBlueDoors/
+echo "Copy done"
 
 
 
